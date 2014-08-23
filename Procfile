84f7100148e9db8c90727800f9119d122b7269f1
Kyle Tolle <kyle@nullsix.com>
2014-08-23 16:09:12 -0600

    Add way to run the server

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec ruby lib/color_divider/api.rb -p $PORT
+
--- lib/color_divider/api.rb
+++ lib/color_divider/api.rb
@@ -3,12 +3,20 @@ require 'color_divider'
 
 class ColorDivider
   class API < Sinatra::Base
+    # Idea from http://www.recursion.org/2011/7/21/modular-sinatra-foreman
+    configure do
+      set :app_file, __FILE__
+      set :port, ENV['PORT']
+    end
+
     get '/middle_color' do
       start_color = params[:start_color]
       end_color   = params[:end_color]
 
       ColorDivider.new(start_color, end_color).middle_color
     end
+
+    run! if app_file == $0
   end
 end
 
